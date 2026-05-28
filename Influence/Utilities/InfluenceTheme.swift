fc78f193bcf47f3ce4de5cb4b39d7be6d9ba0f01
mmay4au-dev <mmay4au@gmail.com>
2026-05-28 14:08:05 -0500

    Add Influence MVP files

---
+++ Influence/Utilities/InfluenceTheme.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+enum InfluenceTheme {
+    static let background = Color(red: 0.05, green: 0.06, blue: 0.08)
+    static let panel = Color(red: 0.10, green: 0.12, blue: 0.15)
+    static let panelElevated = Color(red: 0.14, green: 0.16, blue: 0.20)
+    static let influence = Color(red: 0.94, green: 0.72, blue: 0.31)
+    static let civic = Color(red: 0.40, green: 0.70, blue: 0.86)
+    static let success = Color(red: 0.35, green: 0.77, blue: 0.55)
+    static let textSecondary = Color.white.opacity(0.68)
+}
